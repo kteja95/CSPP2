ca3537d0cbc98031bc765c84f27af9f721542dc7
ktej95 <kteja1989@msitprogram.net>
2018-08-27 18:28:57 +0530

    helloworld.java

---
+++ cspp2-assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		String name = s.next();
+		int roll = s.nextInt();
+		System.out.println("Name:" +name+ " Roll:" +roll);
+
+	}
+}
