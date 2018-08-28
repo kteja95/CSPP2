eb3ad06d35c5ac1cfa661e29b2d4120c0415a766
ktej95 <kteja1989@msitprogram.net>
2018-08-28 19:17:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -4,10 +4,10 @@ Main class
 */
 public class Solution
 {
-	/*
-	Do not modify this main function.
-	*/
-	private Solution() {
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
         /**
            *@constructor
         */
@@ -16,25 +16,25 @@ public class Solution
     /**.
         * @param  args the argments
         */
-	public static void main(String[] args) {	
-	//THIS IS THE MAIN FUNCTION//
-		Scanner s=new Scanner(System.in);
+    public static void main(String[] args) {    
+    //THIS IS THE MAIN FUNCTION//
+        Scanner s=new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result=power(base,exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	/**.
+    }
+    /*
+    Need to write the power function and print the output.
+    */
+    /**.
     Need to write the rootsOfQuadraticEquat
     @param    base   the base
     @param    exponent  the exponent
-	*/
-	static long power(int base,int exponent) {
-		if (exponent>0)
-			return base*power(base,exponent-1);
-		return 1;
-	}
+    */
+    static long power(int base,int exponent) {
+        if (exponent > 0)
+            return base * power(base, exponent - 1);
+        return 1;
+    }
 }
