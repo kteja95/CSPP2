c13e4860e662c563955cc24238f7719eedac834b
ktej95 <kteja1989@msitprogram.net>
2018-08-28 19:58:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -23,11 +23,8 @@ final class Solution {
         long result = power(base, exponent);
         System.out.println(result);
     }
-    /*
-    Need to write the power function and print the output.
-    */
     /**.
-    Need to write the rootsOfQuadraticEquat
+    Need to write the power function and print the output.
     @param    base   the base
     @param    exponent  the exponent
     */
