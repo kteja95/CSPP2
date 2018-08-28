7f264e724b971f204c1102f8c2bcce6eecf3228c
ktej95 <kteja1989@msitprogram.net>
2018-08-28 18:45:31 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 // import java.lang.Math;
 
-public final class Solution
-{
+public final class Solution{
 	/*
 	Do not modify this main function.
 	*/
-	private Solution()
-	{
+	private Solution(){
 		/* This is the constructor*/
 
 	}
-	public static void main(final String[] args)
-	{/*THIS IS THE MAIN FUNCTION*/
+	public static void main(final String[] args){
+		/*THIS IS THE MAIN FUNCTION*/
 		Scanner scan = new Scanner(System.in);
 		double a = scan.nextInt();
 		double b = scan.nextInt();
@@ -23,8 +21,8 @@ public final class Solution
 	Need to write the rootsOfQuadraticEquat
 	ion function and print the output.
 	*/
-	static void rootsOfQuadraticEquation(final double a, final double b, final double c)
-	{/**
+	static void rootsOfQuadraticEquation(final double a, final double b, final double c){
+	/**
 		* square roots
 		*/
 		double root1;
