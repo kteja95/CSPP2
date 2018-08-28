79b104651e745d59588be4d94edd3d1a6db4b749
ktej95 <kteja1989@msitprogram.net>
2018-08-28 18:57:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -2,33 +2,34 @@ import java.util.Scanner;
 // import java.lang.Math;
 
 public final class Solution {
-	/**.
-	Do not modify this main function.
-	**/
-	private Solution() {
-		/** This is the constructor**/
+    /**.
+    Do not modify this main function.
+    **/
+    private Solution() {
+        /** This is the constructor**/
 
-	}
-	public static void main(final String[] args) {
-		/**.THIS IS THE MAIN FUNCTION**/
-		Scanner scan = new Scanner(System.in);
-		double a = scan.nextInt();
-		double b = scan.nextInt();
-		double c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquat
-	ion function and print the output.
-	*/
-	static void rootsOfQuadraticEquation(final double a, final double b, final double c) {
-	/**.
-		 square roots
-		**/
-		double root1;
-		double root2;
-		root1 = (-b + (Math.sqrt(Math.abs(b * b) - 2 * 2 * a * c))) / (2 * a);
-		root2 = (-b - (Math.sqrt(Math.abs(b * b) - 2 * 2 * a * c))) / (2 * a);
-		System.out.println(root1 + " " + root2);
-	}
+    }
+    public static void main(final String[] args) {
+        /**.THIS IS THE MAIN FUNCTION**/
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextInt();
+        double b = scan.nextInt();
+        double c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquat
+    ion function and print the output.
+    */
+    static void rootsOfQuadraticEquation(final double a, final double b, final double c) {
+    /**.
+         square roots
+        **/
+        double r1;
+        double r2;
+        int f = 4;
+        r1 = (-b + (Math.sqrt(Math.abs(b * b) - f * a * c))) / (2 * a);
+        r2 = (-b - (Math.sqrt(Math.abs(b * b) - f * a * c))) / (2 * a);
+        System.out.println(r1 + " " + r2);
+    }
 }
