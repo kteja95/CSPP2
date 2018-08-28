3d77615ef629d4291673477fa80345ea2588925d
ktej95 <kteja1989@msitprogram.net>
2018-08-28 19:22:56 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -18,10 +18,10 @@ public class Solution
         */
     public static void main(String[] args) {    
     //THIS IS THE MAIN FUNCTION//
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         final int base = s.nextInt();
         final int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
     }
     /*
@@ -32,7 +32,7 @@ public class Solution
     @param    base   the base
     @param    exponent  the exponent
     */
-    static long power(final int base,final int exponent) {
+    static long power(final int base, final int exponent) {
         /**.
         POWER*/
         if (exponent > 0) {
