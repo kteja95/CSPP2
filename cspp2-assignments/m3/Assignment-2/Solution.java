e880ba3537ec1241bf1aec91518dda30a7c4ea5a
ktej95 <kteja1989@msitprogram.net>
2018-08-29 14:05:47 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -15,19 +15,17 @@ public class Solution {
         int count = 0;
         final int f = 10;
         final int var = 7;
-        for(int i = 0; i <= n; i++)
-        {
+        for(int i = 0; i <= n; i++) {
         	int temp = i;
         	while(temp > 0)
         	{
-        		if (temp%10 ==var)
+        		if (temp % f ==var)
         		{
         			count = count + 1;
         		}
         	temp = Math.round(temp/f);
         	}
-        	
-        }
+}
     System.out.println(count);
 
     }
