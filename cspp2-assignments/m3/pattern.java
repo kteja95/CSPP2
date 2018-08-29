be1434f0088471fb35eac8875433ee3f2bfe83b4
ktej95 <kteja1989@msitprogram.net>
2018-08-29 11:24:12 +0530

    patterns numbers

---
+++ cspp2-assignments/m3/pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class patterns
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter the limit for the pattern");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1;i<=n;i++){
+           for(int j=1;j<=i;j++){
+                System.out.print(j);
+           }
+            System.out.println();
+             
+        }
+            
+        
+        
+    }
+}
