ebd5b1fb303a3c31e413a690c8ae0fdfbebe24b2
ktej95 <kteja1989@msitprogram.net>
2018-08-29 14:01:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -7,11 +7,10 @@ public class Solution {
 /**. Fill the main function to print the number of 7's between 1 to n
 */
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
 
   		// System.out.println("Enter the limit");
-        Scanner s=new Scanner(System.in);      
+        Scanner s=new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         final int f = 10;
