e0b29607410bbaafeab862990bd2c9ff79251141
ktej95 <kteja1989@msitprogram.net>
2018-08-29 14:11:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -1,36 +1,39 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
+/**.
+    solution the class
+*/
 final class Solution {
 /**. Fill the main function to print the number of 7's between 1 to n
 */
-	private Solution() {
-		/**.
-		    @constructor
-		    */
-	}
+    private Solution() {
+        /**.
+            @constructor
+            */
+    }
 /**.
    @param args The arguments
-   */ 
+*/ 
 
     public static void main(final String[] args) {
 
-  		// System.out.println("Enter the limit");
+        // System.out.println("Enter the limit");
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         final int f = 10;
         final int var = 7;
         for (int i = 0; i <= n; i++) {
-        	int temp = i;
-        	while (temp > 0) {
-        		if (temp % f == var) {
-        			count = count + 1;
-        		}
-        	temp = Math.round(temp / f);
-        	}
+            int temp = i;
+            while (temp > 0) {
+                if (temp % f == var) {
+                    count = count + 1;
+                }
+            temp = Math.round(temp / f);
+            }
 }
     System.out.println(count);
 
