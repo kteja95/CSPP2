ac6f8edb49f9a263b2f2ac610b51e70a80a85acd
ktej95 <kteja1989@msitprogram.net>
2018-08-29 14:45:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -30,10 +30,8 @@ final class Solution {
     @param n1   the first number
     @param n2   the secind number
 */
+/**. @return it returns the gcd*/    
     static int gcd(final int n1, final int n2) {
-/**
-    @ return   it returns the gcd
-*/
          int temp = 1;
         for (int i = 1; i <= n1 && i <= n2; i++) {
             if (n1 % i == 0 && n2 % i == 0) {
