9ce10b37ed979b8791f69e90f09395e7811fc4ca
ktej95 <kteja1989@msitprogram.net>
2018-08-30 15:25:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 */
 final class Solution {
 /**.
-Do not modify the main function 
+Do not modify the main function
 */
     private Solution() {
 /**.
@@ -12,7 +12,6 @@ Do not modify the main function
     }
 /**.
 @param args The arguments
-@return return the string
 */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,10 +20,14 @@ Do not modify the main function
         System.out.println(reverse);
 
     }
-//Write reverseString function
+/**.
+@reverseString the function to reverse a given string.
+@return returns the string back to main
+*/
+
     static String reverseString(final String s) {
         String newstring = "";
-        for(int i = s.length() - 1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             newstring += s.charAt(i);
         }
         return newstring;
