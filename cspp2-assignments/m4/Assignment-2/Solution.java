b221593b3054d8560601580f9a3a0811b086070d
ktej95 <kteja1989@msitprogram.net>
2018-08-30 14:00:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,54 +1,48 @@
 import java.util.*;
-public class Solution {
+/**. Solution the class
+*/
+final class Solution {
     /* Fill the main function to print resultant of addition of matrices*/
     private Solution()
     {
-        /**.@constructor
+        /**.
+        @constructor
         */
     }
     /**.
     @param args
     */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int m = s.nextInt();
         int[][] arr = new int[n][m];
-        for (int i = 0 ;i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+        for (int i = 0 ;i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = s.nextInt();
             }
         }
         int p = s.nextInt();
         int q = s.nextInt();
         int[][] ar = new int[p][q];
-        for (int i = 0; i < p; i++)
-        {
-            for (int j = 0; j<q; j++)
-            {
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j<q; j++) {
                 ar[i][j] = s.nextInt();
             }
         }
         int[][] newarray = new int[n][m];
-    if (n == p && m == q)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
+    if (n == p && m == q) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 newarray[i][j] = arr[i][j] + ar[i][j];
             }
         }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if(j==m-1)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(j == m - 1)
                     System.out.print(newarray[i][j]);
                 else
-                    System.out.print(newarray[i][j]+" ");
+                    System.out.print(newarray[i][j] + " ");
             }
         System.out.println();
         }
@@ -56,7 +50,5 @@ public class Solution {
     }
     else
         System.out.println("not possible");
-        
-        
     }
 }
