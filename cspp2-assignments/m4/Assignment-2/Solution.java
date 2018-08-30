5d258a69b218bb2ed769fc428c7e5ccc2b157022
ktej95 <kteja1989@msitprogram.net>
2018-08-30 12:14:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,44 +1,52 @@
 import java.util.*;
 public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = Integer.parseInt(s.nextLine());
-		int m = Integer.parseInt(s.nextLine());
-		int[][] arr = new int[n][m];
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m;j++)
-			{
-				arr[i][j] = s.nextInt();
-			}
-		}
-		int p = s.nextInt();
-		int q = Integer.parseInt(s.nextLine());
-		int[][] ar = new int[p][q];
-		for(int i=0;i<p;i++)
-		{
-			for(int j=0;j<q;j++)
-			{
-				ar[i][j] = s.nextInt();
-			}
-		}
-		if(n==p && m==q)
-		{
-			int[][] newarray = new int[n][m];
-			for(int i=0;i<n;i++)
-			{
-				for(int j =0;j<m;j++)
-				{
-					newarray[i][j]=arr[i][j]+ar[i][j];
-				}
-			}
-		}
-		else
-		{
-			System.out.println("not possible");
-		}
-		
-		
-	}
+    /* Fill the main function to print resultant of addition of matrices*/
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                arr[i][j] = s.nextInt();
+            }
+        }
+        int p = s.nextInt();
+        int q = s.nextInt();
+        int[][] ar = new int[p][q];
+        for(int i=0;i<p;i++)
+        {
+            for(int j=0;j<q;j++)
+            {
+                ar[i][j] = s.nextInt();
+            }
+        }
+        int[][] newarray = new int[n][m];
+        if(n==p && m==q)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j =0;j<m;j++)
+                {
+                    newarray[i][j]=arr[i][j]+ar[i][j];
+                }
+            }
+        }
+        else
+        {
+            System.out.println("not possible");
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                System.out.println(newarray[i][j]);
+            }
+        }
+
+        
+        
+    }
 }
