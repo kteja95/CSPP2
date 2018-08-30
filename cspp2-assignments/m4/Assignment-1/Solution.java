b57ee9ed9ed6c84370c070ba99aee4dce6614ba3
ktej95 <kteja1989@msitprogram.net>
2018-08-30 11:31:57 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -1,26 +1,27 @@
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 final  class Solution {
 /*
-	Fill this main function to print maximum of given array
-	*/
-	private Solution() {
-		/**.
-		@constructor
-		*/
-	}
-	/**.
-	@param args THE ARGUMENTS
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		System.out.println(arr[arr.length - 1]);
+    Fill this main function to print maximum of given array
+    */
+    private Solution() {
+        /**.
+        @constructor
+        */
+    }
+    /**.
+    @param args THE ARGUMENTS
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - 1]);
 
-	}
+    }
 }
