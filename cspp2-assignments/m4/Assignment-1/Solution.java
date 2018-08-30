288b8bda550a8368d80052af5e0f977b5351c7e4
ktej95 <kteja1989@msitprogram.net>
2018-08-30 11:24:36 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -8,7 +8,13 @@ public class Solution
 	{
 		Scanner sc=new Scanner(System.in);
 		int n= sc.nextInt();
-		
+		int[] arr = new int[n];
+		for(int i =0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
 
 	}
 }
