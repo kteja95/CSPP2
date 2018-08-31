e3877983a56f546ab1f28ec9e505bbdc3c3d30f6
ktej95 <kteja1989@msitprogram.net>
2018-08-31 16:05:37 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,4 +1,4 @@
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+//Convert Binary to Decimal
 import java.util.Scanner;
 /**. class Solution is the class for binary to decimal*/
 final class Solution {
