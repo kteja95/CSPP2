25f0779e3204e6e217ff75a1f13e0bf5f1f017fc
ktej95 <kteja1989@msitprogram.net>
2018-09-04 17:09:51 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- cspp2-assignments/CodeCampm8/CodecampList/List.java
+++ cspp2-assignments/CodeCampm8/CodecampList/List.java
@@ -63,7 +63,7 @@ final class List {
     */
     private int size;
     /**. @g the size*/
-    final int g = 10;
+    private final int g = 10;
     /**. @arr the array*/
     private int[] arr = new int[g];
     /**. List the constructor*/
