8fe0a17b093ab6e83d9000195f601f29f19baf36
ktej95 <kteja1989@msitprogram.net>
2018-09-04 15:16:40 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- cspp2-assignments/CodeCampm8/CodecampList/List.java
+++ cspp2-assignments/CodeCampm8/CodecampList/List.java
@@ -73,7 +73,7 @@ public class List {
         // How many items do we have in the list when you create it?
         // An empty list has how many items?
         // That is the initial value to use for size.
-        this.size=0;
+        this.size = 0;
 
 
     }
@@ -91,8 +91,8 @@ public class List {
      */
     public void add(int item) {
         //Inserts the specified element at the end of the list.
-        arr[size]=item;
-        size+=1;
+        arr[size] = item;
+        size += 1;
     }
 
     /*
@@ -130,28 +130,28 @@ public class List {
     public void remove(int index) {
         // write the logic for remove here.
         // Think about what to do to the size variable.
-        if(index<size) {
-            int x=arr[index];
-            int flag=0;
-            int pos =0;
-            for(int j=0;j<size;j++)
+        if(index < size) {
+            int x = arr[index];
+            int flag = 0;
+            int pos = 0;
+            for(int j = 0; j < size; j++)
             {
-                if(arr[j]==x)
+                if(arr[j] == x)
                 {
-                    flag=1;
-                    pos=j;
+                    flag = 1;
+                    pos = j;
                     break;
                 }
                 else
-                    flag=0;
+                    flag = 0;
             }
-            size-=1;
-            if(flag==1)
+            size -= 1;
+            if(flag == 1)
             {
-                for(int i=pos+1;i<=size;i++)
-                    arr[i-1]=arr[i];
+                for(int i = pos+1; i <= size; i++)
+                    arr[i-1] = arr[i];
             }
-            arr[size]=0;
+            arr[size] = 0;
         }
         else
             System.out.println("Invalid Position Exception");
@@ -170,7 +170,7 @@ public class List {
      */
     public int get(int index) {
         // Replace the code below to write the code for get
-        if(index<size)
+        if(index < size)
         {
             return arr[index];
         }
@@ -199,15 +199,15 @@ public class List {
      */
     public String toString() {
         // Replace the code below
-        String finalarr="[";
-        if(size>0)
+        String finalarr = "[";
+        if(size > 0)
         {
-            for(int j=0;j<size-1;j++)
+            for(int j = 0; j < size-1; j++)
         {
-            finalarr+=arr[j]+",";
+            finalarr += arr[j] + ",";
             
         }
-        return finalarr+arr[size-1]+"]";           
+        return finalarr + arr[size-1] + "]";
         }
         
         return "print the list";
@@ -221,13 +221,13 @@ public class List {
      */
     public boolean contains(int item) {
         // Replace the code below
-        int itemcount =0;
-        for(int i=0;i<size;i++)
+        int itemcount = 0;
+        for(int i = 0; i < size; i++)
         {
-            if(item==arr[i])
-                itemcount+=1;
+            if(item == arr[i])
+                itemcount += 1;
         }
-        if(itemcount>0)
+        if(itemcount > 0)
             return true;
         return false;
     }
@@ -239,7 +239,7 @@ public class List {
      */
     public int indexOf(int item) {
         // Replace the code below
-        for(int i=0;i<size;i++)
+        for(int i = 0; i<size; i++)
         {
             if(item == arr[i])
                 return i;
